0ccdeb4bac5daca4066da9c0aa52a7406b32b4f2
nguyentienkhuong1104 <nguyentienkhuong1104@gmail.com>
2019-05-24 10:25:18 +0700

    modifie reg

--- recognize_video.py
+++ recognize_video.py
@@ -1,6 +1,6 @@
 # USAGE
 # python3.5 recognize_video.py --detector face_detection_model \
-# 	--embedding-model openface_nn4.small2.v1.t7 \
+# 	--embedding-model face_small.t7 \
 # 	--recognizer output/recognizer.pickle \
 # 	--le output/le.pickle
 
